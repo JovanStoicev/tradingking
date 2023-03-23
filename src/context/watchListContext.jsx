e1ef21dc8a590f2b81b9730db0b3ef34103b2097
Jovan Stoicev <67899512+JovanStoicev@users.noreply.github.com>
2023-03-23 15:20:43 +0000

    added details for stock in StockDetailPage

--- src/context/watchListContext.jsx
+++ src/context/watchListContext.jsx
@@ -1,22 +1,28 @@
-import { createContext, useState } from "react"
+import { createContext, useState, useEffect } from "react"
 
 export const WatchListContext = createContext()
 
 export const WatchListContextProvider = (props) => {
+  const [watchList, setWatchList] = useState(
+    localStorage.getItem("watchList").split(",")
+    // ["GOOGL", "MSFT", "AMZN", "TSLA"]
+  )
   
-  const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN", "TSLA"])
+  useEffect(() => {
+    localStorage.setItem("watchList", watchList)
+  }, [watchList])
 
   const addStock = (stock) => {
-    if(watchList.indexOf(stock) === -1) {
+    if (watchList.indexOf(stock) === -1) {
       setWatchList([...watchList, stock])
     }
   }
 
   const deleteStock = (stock) => {
-    setWatchList (watchList.filter((el) => {
+    setWatchList(watchList.filter((el) => {
       return el !== stock
     }))
   }
-  
-  return <WatchListContext.Provider value={{ watchList, addStock, deleteStock }}>{ props.children }</WatchListContext.Provider>
+
+  return <WatchListContext.Provider value={{ watchList, addStock, deleteStock }}>{props.children}</WatchListContext.Provider>
 }
